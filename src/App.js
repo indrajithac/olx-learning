4dc90a1b532c87cfccb895a6aa5a26455ab7e3e4
Indrajith A C <indrajithac@gmail.com>
2021-12-24 11:35:30 +0530

    signup form added to state,set route,firebase intergrated with some error

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 /**
  * ?  =====Import Components=====
  */
 import Home from './Pages/Home';
+import Signup from './Components/Signup/Signup';
 
 function App() {
   return (
     <div>
-      <Home />
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/signup' element={<Signup />} />
+        </Routes>
+
+
+      </Router>
     </div>
   );
 }
---
+++ src/firebase/config.js
@@ -0,0 +1,14 @@
+import * as firebase from "firebase/app"
+require('firebase/auth')
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDLJAvi_QRJ098D8DgUjlohs3xM36aJWmM",
+    authDomain: "olx-clone-97ae9.firebaseapp.com",
+    projectId: "olx-clone-97ae9",
+    storageBucket: "olx-clone-97ae9.appspot.com",
+    messagingSenderId: "348073976620",
+    appId: "1:348073976620:web:eb5a7570dc91effdf08289",
+    measurementId: "G-0P0WR1F3WR"
+  };
+
+export default firebase.initializeApp(firebaseConfig)
